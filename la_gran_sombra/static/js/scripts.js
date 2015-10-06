44d4e59ddf330ba9351503cadd52659bb7603870
diegoduncan21 <diegoduncan21@gmail.com>
2015-10-05 23:15:18 -0300

    arreglando la redireccion

---
+++ la_gran_sombra/static/js/scripts.js
@@ -0,0 +1,38 @@
+$(function() {
+    $('#suma').click(function(e) {
+        var minutes = parseInt($('#id_minutes').val());
+        $('#id_minutes').val(minutes+1);
+    });
+    $('#resta').click(function(e) {
+        var minutes = parseInt($('#id_minutes').val());
+        if(minutes>0){
+            $('#id_minutes').val(minutes-1);
+        }
+    });
+    $('#play').click(function(e) {
+        e.preventDefault();
+        console.log(e);
+        var minutes = $('#id_minutes').val();
+        setTimeout(function(){
+            // sonido}
+            var music = new Audio("/static/sounds/alert.mp3");
+            music.play();
+            var counter = 3;
+            $(music).bind("ended", function() {
+                music.play();
+                if (counter < 1) {
+                    $('#timer_form').submit()
+                }
+                counter -= 1;
+            });
+
+        }, (minutes * 60000));
+
+        // console counter
+        var counter = 0;
+        setInterval(function() {
+            counter += 1;
+            $('#id_minutes').val(counter);
+        }, 1000)
+    })
+});
